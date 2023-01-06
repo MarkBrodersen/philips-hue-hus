3632c23deb10792bb7fb7e9ce781513fe4356789
Mike <U10604@rts-365.dk>
2023-01-06 11:46:55 +0100

    added some logos

--- src/pages/NewTheme.js
+++ src/pages/NewTheme.js
@@ -1,7 +1,7 @@
 import Container from "../components/Container";
 import Sheet from "react-modal-sheet";
 import {useState} from "react";
-import {X, BedSingle, Monitor, ChefHat, Sofa, Home} from "lucide-react"
+import {X, BedSingle, Monitor, Tent, Sofa, Home, VenetianMask, ShowerHead, UtensilsCrossed} from "lucide-react"
 
 const NewTheme = () => {
     const [isOpen, setOpen] = useState(false)
@@ -15,7 +15,7 @@ const NewTheme = () => {
         </div>
         <div className="w-32">
         <Container>
-           <button onClick={() => setOpen(true)}>Choose Logo</button>
+           <button  onClick={() => setOpen(true)}>Choose Logo</button>
         </Container>
         </div>
         <Sheet isOpen={isOpen} detent='full-height' onClose={() => setOpen(false)}>
@@ -23,12 +23,15 @@ const NewTheme = () => {
             <Sheet.Header className="bg-zinc-900"/> 
             <Sheet.Content className="bg-zinc-900 ">
             <button className="absolute top-0 right-5" onClick={() => setOpen(false)}><X color="white" size={28}/></button>
-                   <div className="grid grid-3  grid-flow-col grid-rows-2 pt-12 gap-8">
+                   <div className="grid grid-3 w-80 m-auto grid-flow-col grid-rows-3 pt-12 gap-14">
                     <BedSingle color="white" size={48}/>
                     <Monitor  color="white" size={48}/>
-                    <ChefHat  color="white"size={48}/>
+                    <UtensilsCrossed  color="white"size={48}/>
                     <Sofa color="white" size={48}/>
                     <Home color="white" size={48}/>
+                    <VenetianMask color="white" size={48}/>
+                    <ShowerHead color="white" size={48}/>
+                    <Tent color="white" size={48}/>
                     </div>  
             </Sheet.Content>
         </Sheet.Container>
