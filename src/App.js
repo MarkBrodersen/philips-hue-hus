9c5795b4ac6ef3d58e89e9241273a7b6ce68935c
Mark Brodersen <U2405963@rts-365.dk>
2023-01-07 11:49:06 +0100

    Missing navigate backwords otherwise its finish

--- src/App.js
+++ src/App.js
@@ -1,72 +1,68 @@
-import axios from 'axios'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import Layout from './Layout'
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Layout from "./Layout";
 
 function App() {
-	const [token, setToken] = useState('')
-	const username = localStorage.getItem('username')
-	const [loading, setLoading] = useState(true)
+  const [token, setToken] = useState("");
+  const username = localStorage.getItem("username");
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		if (username) {
-			setLoading(true)
-			axios.get(`http://192.168.8.100/api/${username}`).then(res => {
-				setLoading(false)
-				if (res.data.error) {
-					setToken(undefined)
-				} else {
-					setToken(username)
-				}
-			})
-		} else {
-			setLoading(false)
-		}
-	}, [])
+  useEffect(() => {
+    if (username) {
+      setLoading(true);
+      axios.get(`http://192.168.8.100/api/${username}`).then((res) => {
+        setLoading(false);
+        if (res.data.error) {
+          setToken(undefined);
+        } else {
+          setToken(username);
+        }
+      });
+    } else {
+      setLoading(false);
+    }
+  }, []);
 
-	return (
-		// <div className='bg-zinc-900 min-h-screen p-12'>
-		// 	<Container fit>
-		// 		<h1 className='h-60 w-80'>Hej</h1>
-		// 	</Container>
-		// </div>
-		<>
-			<ToastContainer
-				position='top-right'
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme='dark'
-			/>
-			<BrowserRouter>
-				<Routes>
-					{loading ? (
-						<Route path='/' element={<Layout />}>
-							<Route index element={<div>Loading...</div>} />
-						</Route>
-					) : token ? (
-						<Route path='/' element={<Layout />}>
-							<Route index element={<Home />} />
-						</Route>
-					) : (
-						<Route path='/' element={<Layout />}>
-							<Route index element={<SignIn />} />
-						</Route>
-					)}
-				</Routes>
-			</BrowserRouter>
-		</>
-	)
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <BrowserRouter>
+        <Routes>
+          {loading ? (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<div>Loading...</div>} />
+            </Route>
+          ) : token ? (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/rooms" element={<Home />} />
+            </Route>
+          ) : (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<SignIn />} />
+            </Route>
+          )}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/templates/Navigation.js
+++ src/templates/Navigation.js
@@ -1,12 +1,39 @@
+import { useLocation, Navigate } from "react-router-dom";
 import { ChevronLeft } from "lucide-react";
-import BurgerMenuIcon from "../components/sub-components/BurgerMenuIcon";
+import BurgerMenu from "../components/sub-components/BurgerMenu";
 
 const Navigation = () => {
+  let place = useLocation().pathname;
+
   return (
-    <header className="w-full p-4 flex justify-between  fixed top-0">
-      <ChevronLeft className="w-7 h-7" />
-      <h1>Home</h1>
-      <BurgerMenuIcon />
+    <header className="w-full p-4 pr-6 flex justify-between  fixed top-0">
+      {place !== "/" ? (
+        <button>
+          <Navigate to="/">
+            <ChevronLeft className="w-7 h-7" />
+          </Navigate>
+        </button>
+      ) : (
+        <p className="w-[28px] h-[28px]"></p>
+      )}
+      {place === "/" ? (
+        <h1>Home</h1>
+      ) : (
+        (place = "/rooms" ? (
+          <h1>Rooms</h1>
+        ) : (
+          (place = "/newroom" ? (
+            <h1>New Room</h1>
+          ) : (
+            (place = "/themes" ? (
+              <h1>Themes</h1>
+            ) : (
+              (place = "/newtheme" ? <h1>New Theme</h1> : null)
+            ))
+          ))
+        ))
+      )}
+      <BurgerMenu />
     </header>
   );
 };
