f7e66d4787352dbeea46d34092c4ef6f2c1c2754
Mike <U10604@rts-365.dk>
2023-01-09 10:44:02 +0100

    selected icon, onto color picker

--- src/pages/NewTheme.js
+++ src/pages/NewTheme.js
@@ -2,10 +2,15 @@ import Container from "../components/Container";
 import Sheet from "react-modal-sheet";
 import {useState} from "react";
 import {X, BedSingle, Monitor, Tent, Sofa, Home, VenetianMask, ShowerHead, UtensilsCrossed, Lightbulb} from "lucide-react"
-
+import { SketchPicker } from "react-color";
 const NewTheme = () => {
     const [isOpen, setOpen] = useState(false)
-    const [Icon, setIicon] = useState('');
+    const [Icon, setIcon] = useState('');
+    const [isColorOpen, setColorOpen] = useState(false)
+    function iconPicker(icon) {
+        setIcon(icon)
+        setOpen(false)
+    }
     return ( 
     <div>
         <div className="w-72 m-auto pt-8">
@@ -14,10 +19,31 @@ const NewTheme = () => {
         </Container>
        
         </div>
-        <div className="w-32">
+        <div className="w-32 ml-8 mt-6">
         <Container fit>
             <div className="p-4 ">
-            <button  onClick={() => setOpen(true)}><Lightbulb color="white" size={28}/></button>
+            <button  onClick={() => setOpen(true)}>
+                {Icon === "bedsingle" ? (
+                    <BedSingle color="white" size={28}/>
+                ) : Icon === "Monitor" ? (
+                    <Monitor color="white" size={28}/>
+                ) : Icon === "utensilscrossed" ? (
+                    <UtensilsCrossed color="white" size={28}/>
+                ) : Icon === "sofa" ? (
+                    <Sofa color="white" size={28}/>
+                ) : Icon === "home" ? (
+                    <Home color="white" size={28}/>
+                ) : Icon === "venetianmask" ? (
+                    <VenetianMask color="white" size={28}/>
+                ) : Icon === "showerhead" ? (
+                    <ShowerHead color="white" size={28}/>
+                ) : Icon === "tent" ? (
+                    <Tent color="white" size={28}/>
+                ) : (
+                    <Lightbulb color="white" size={28}/>
+                    )
+            }
+                </button>
             </div>
           
         </Container>
@@ -28,21 +54,35 @@ const NewTheme = () => {
             <Sheet.Content className="bg-zinc-900 ">
             <button className="absolute top-0 right-5" onClick={() => setOpen(false)}><X color="white" size={28}/></button>
                    <div className="grid grid-3 w-80 m-auto grid-flow-col grid-rows-3 pt-12 gap-14">
-                    <form >
-                    <BedSingle color="white" size={48}/>
-                    <Monitor  color="white" size={48}/>
-                    <UtensilsCrossed  color="white"size={48}/>
-                    <Sofa color="white" size={48}/>
-                    <Home color="white" size={48}/>
-                    <VenetianMask color="white" size={48}/>
-                    <ShowerHead color="white" size={48}/>
-                    <Tent color="white" size={48}/>
-                    </form>
-                  
+                 <button onClick={() => iconPicker("bedsingle")}> <BedSingle color="white" size={48}/> </button>
+                 <button onClick={() => iconPicker("Monitor")}> <Monitor  color="white" size={48}/></button>
+                 <button onClick={() => iconPicker("utensilscrossed")}><UtensilsCrossed  color="white"size={48}/></button>
+                 <button onClick={() => iconPicker("sofa")}> <Sofa color="white" size={48}/></button>
+                 <button onClick={() => iconPicker("home")}><Home color="white" size={48}/></button>
+                 <button onClick={() => iconPicker("venetianmask")}> <VenetianMask color="white" size={48}/></button>
+                 <button onClick={() => iconPicker("showerhead")}><ShowerHead color="white" size={48}/></button>
+                 <button onClick={() => iconPicker("tent")}><Tent color="white" size={48}/></button>
+
                     </div>  
             </Sheet.Content>
         </Sheet.Container>
         </Sheet>
+
+                <Container>
+                    <button onClick={() => setColorOpen(true)}>xd</button>
+                </Container>
+                <Sheet isOpen={isColorOpen} detent="content-height" disableDrag={true} onClose={() => setColorOpen(false)}>
+                    <Sheet.Container>
+                        <Sheet.Header className="bg-zinc-900"/>
+                        <Sheet.Content className="bg-zinc-900">
+                        <button className="absolute top-0 right-5" onClick={() => setColorOpen(false)}><X color="white" size={28}/></button>
+                        <SketchPicker />
+                        </Sheet.Content>
+                    </Sheet.Container>
+            
+                </Sheet>
+           
+
     </div> 
     );
 }
